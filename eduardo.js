75ed13d2d2f9cc6dca1b86eb1acc5b81b4a5ea4a
Eduardo Pinheiro Miranda <89249898+EduardoPinheiroMiranda@users.noreply.github.com>
2023-02-22 23:02:49 -0300

    add portifolio

---
+++ eduardo.js
@@ -0,0 +1,28 @@
+const destaques = document.querySelectorAll(".destaque")
+const openDestaque = document.querySelectorall(".caps")
+
+destaques[0].onclick = () => {
+    openDestaque[0].setAttribute("style", "width:100px")
+}
+destaques[1].onclick = () => {
+    alert('oi')
+}
+destaques[2].onclick = () => {
+    alert('oi')
+}
+destaques[3].onclick = () => {
+    alert('oi')
+}
+destaques[4].onclick = () => {
+    alert('oi')
+}
+destaques[5].onclick = () => {
+    alert('oi')
+}
+destaques[6].onclick = () => {
+    alert('oi')
+}
+destaques[7].onclick = () => {
+    alert('oi')
+}
+
