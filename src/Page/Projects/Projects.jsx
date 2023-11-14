cee9d11aa3ac19f859035686c567f4df89609666
Eduardo Pinheiro Miranda <89249898+EduardoPinheiroMiranda@users.noreply.github.com>
2023-11-14 09:08:12 -0300

    corrigindo diretorio de img

--- src/Page/Projects/Projects.jsx
+++ src/Page/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 export const Projects = [
     {
         id: 1,
-        img:"../../src/assets/img/site/rifaOnline.png",
+        img:"./../../src/assets/img/site/rifaOnline.png",
         alt: "Rifa online",
         name: "Rifa online",
         desc: `
