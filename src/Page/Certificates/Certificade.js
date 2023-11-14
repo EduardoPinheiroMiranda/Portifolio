e26bce5a2f8680e31b409100988ee86e68cf84de
Eduardo Pinheiro Miranda <89249898+EduardoPinheiroMiranda@users.noreply.github.com>
2023-11-13 23:03:09 -0300

    corrigindo erro de diretorio

--- src/Page/Certificates/Certificade.js
+++ src/Page/Certificates/Certificade.js
@@ -1,17 +1,17 @@
 export const Certificates = [
     {
         id: 1,
-        src: "../../src/assets/img/Certificados/certificado_git-github.jpg",
+        src: "./src/assets/img/Certificados/certificado_git-github.jpg",
         alt: "Certifica em Git e GitHub",
     },
     {
         id: 2,
-        src: "../../src/assets/img/Certificados/qualidadesoftware.png",
+        src: "./src/assets/img/Certificados/qualidadesoftware.png",
         alt: "Qualidade de software",
     },
     {
         id: 3,
-        src: "../../src/assets/img/Certificados/certificado_git-github.jpg",
+        src: "./src/assets/img/Certificados/certificado_git-github.jpg",
         alt: "Certifica em Git e GitHub",
         desc: `Curso de Git e Git hub avançados`
     }
